9b36bf07fd6d8cae5147b9049b59e0d5bb6b4ce2
Manikantan Narasimhan <rvns03@gmail.com>
2015-07-26 17:29:37 +0530

    Disjoint Set Helper Class Implementation

--- disjoint_set.py
+++ disjoint_set.py
@@ -1,8 +1,47 @@
+__author__ = 'V Manikantan'
+
+documentation = '''
+Helper Class for Disjoint Set data structure
+
+1)  Instantiate object by calling
+
+	obj = Disjoint_Set(n) 
+
+	where n is the number of elements in total. This creates n Disjoint sets with 1 element each.
+
+
+2)	Find takes n as parameter and returns representative element of set to which element number 'n' belongs.
+
+	Function Call: set_rep = obj.Find(n)
+
+3)	Union takes x,y as parameters and merges the sets (if disjoint), such that set containing y becomes child
+	of set containing x.
+
+	Function Call: obj.Union(x,y)
+
+4)	Check takes x,y as parameters and returns True/False based on whether they belong to same set or not.
+
+	Function Call: same_set = obj.Check(x,y)
+
+5)	Size takes n as parameter and returns size of set containing n.
+
+	Function Call: size = obj.Size(n)
+
+6)  Groups returns the total number of Disjoint Sets at present.
+	
+	Function Call: groups = obj.Groups()
+
+
+NOTE: Invalid parameters to functions => No change is done / NoneType objects returned
+
+'''
+
 class Disjoint_Set:
 	def __init__(self,n):
 		self.parents = [i for i in range(n)]
 		self.set_size = [1 for i in range(n)]
 		self.size = n
+		self.groups = n
 
 	def Find(self,n):
 		if(n>=1 and n<=self.size):
@@ -11,18 +50,19 @@ class Disjoint_Set:
 			return None
 
 	def Union(self,x,y):
-		if(x>=1 and x<=self.size and y>=1 and y<=self.size):
-			self.__union__(self.parents,x-1,y-1)
+		if(x>=1 and x<=self.size and y>=1 and y<=self.size and not self.Check(x,y)):
+			self.__union__(self.parents,self.Find(x)-1,self.Find(y)-1)
+			self.groups-=1
 
 	def Check(self,x,y):
 		if(x>=1 and x<=self.size and y>=1 and y<=self.size):
-			return Find(self,x-1)==Find(self,y-1)
+			return self.Find(x)==self.Find(y)
 		else:
 			return None
 
 	def Size(self,n):
 		if(n>=1 and n<=self.size):
-			return self.set_size[n-1]
+			return self.set_size[self.Find(n)-1]
 		else:
 			return None
 
@@ -30,10 +70,12 @@ class Disjoint_Set:
 		if(parent[n]==n):
 			return n
 		else:
-			parent[n] = __find__(parent,parent[n])
+			parent[n] = self.__find__(parent,parent[n])
 			return parent[n]
 
-	def __union__(parent,x,y):
-
-
-
+	def __union__(self,parent,x,y):
+		self.set_size[x]+=self.set_size[y]
+		parent[y] = x
+	
+	def Groups(self):
+		return self.groups
